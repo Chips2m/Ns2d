4c53f8f5211011835665ed50df91110eb383d9a7
Chips2m <34765478+Chips2m@users.noreply.github.com>
2017-12-22 11:16:44 +0100

    Update module_discretization.f90

--- module_discretization.f90
+++ module_discretization.f90
@@ -1,74 +1,80 @@
-module discr
-
+module module_discr
+  use module_mesh
 contains
 
 
-  subroutine gradient(x_c,y_c,fi,dx_fi,dy_fi,n_x,n_y)
+  subroutine gradient(mesh,fi,dx_fi,dy_fi)
     implicit none
-    real(kind=8),dimension(:)  ,allocatable :: x_c,y_c
+    type(tmesh) :: mesh
     real(kind=8),dimension(:,:),allocatable :: fi,dx_fi,dy_fi,p
-    integer :: i,j,k,n_x,n_y
-    real(kind=8) :: h_x,h_y
+    integer :: i,j,k,nx,ny
+    real(kind=8) :: hx,hy
 
-    do i=0,n_x
-	do j=0,n_y+1
-	dx_fi(i,j)=(p(i+1,j)-p(i,j))/(x_c(i+1)-x_c(i))
+    nx = mesh%nx
+    ny = mesh%ny
+    hx = mesh%hx
+    hy = mesh%hy
+    do i=0,nx
+       do j=0,ny+1
+          dx_fi(i,j)= (fi(i+1,j)-fi(i,j))/hx
         end do
     end do
 
-    do i=0,n_x
-	do j=0,n_y+1
-    
-	dy_fi(i,j)=(p(i,j+1)-p(i,j))/(y_c(j+1)-y_c(j))
+    do i=0,nx+1
+       do j=0,ny
+          dy_fi(i,j)=(fi(i,j+1)-fi(i,j))/hy
         end do
     end do  	
     
+  end subroutine gradient
+
+  subroutine divergence(mesh,u,v,div)
+    implicit none
+    type(tmesh) :: mesh
+    real(kind=8),dimension(:,:),allocatable :: u,v,div
+    integer :: i,j,nx,ny
+    real(kind=8) :: hx,hy
     
+    nx = mesh%nx
+    ny = mesh%ny
+    hx = mesh%hx
+    hy = mesh%hy
+    do i=1,nx
+       do j=1,ny
+          div(i,j)= (u(i,j)-u(i-1,j))/hx + (v(i,j)-v(i,j-1))/hy
+        end do
+     end do
+     
+   end subroutine divergence
 
-   end subroutine gradient
-
-
-
-   subroutine divergence(x_f,y_f,d_v,n_x,n_y)
-     implicit none
-     real(kind=8),dimension(:)  ,allocatable :: x_f,y_f
-     real(kind=8),dimension(:,:),allocatable :: d_v,u,v	
-	  integer :: i,j,k,n_x,n_y
-     real(kind=8) :: h_x,h_y
-
-    do i=0,n_x
-	do j=0,n_y+1
-	d_v(i,j)=(  u(i,j) - u(i-1,j) ) / ( x_f(i+1) - x_f(i)  ) + ( v(i+1,j) - v(i,j)  ) / ( y_f(j+1) - y_f(j)  )
-        
-	end do
+   subroutine Linear(mesh,u,v,lu,lv)
+    implicit none
+    type(tmesh) :: mesh
+    real(kind=8),dimension(:,:),allocatable :: u,v,lu,lv
+    integer :: i,j,nx,ny
+    real(kind=8) :: hx2,hy2
+    
+    nx = mesh%nx
+    ny = mesh%ny
+    hx2 = mesh%hx**2
+    hy2 = mesh%hy**2
+    do i=1,nx-1
+       do j=1,ny
+          lu(i,j)=&
+               + (u(i-1,j  )-2*u(i,j)+u(i+1,j))/hx2 &
+               + (u(i  ,j-1)-2*u(i,j)+u(i,j+1))/hy2
+       end do
     end do
-    end subroutine divergence
+    do i=1,nx
+       do j=1,ny-1
+          lv(i,j)=&
+               + (v(i-1,j  )-2*v(i,j)+v(i+1,j))/hx2 &
+               + (v(i  ,j-1)-2*v(i,j)+v(i,j+1))/hy2
+       end do
+    end do
+    
+    
+  end subroutine Linear
 
 
-
-	subroutine linear(u,v,x_f,y_f,dx_l,dy_l,n_x,n_y)
-     implicit none
-     real(kind=8),dimension(:)  ,allocatable :: x_f,y_f
-     real(kind=8),dimension(:,:),allocatable :: dx_l,dy_l,u,v
-	  integer :: i,j,k,n_x,n_y
-     real(kind=8) :: h_x,h_y
- 
-	     do i=0,n_x
-		do j=0,n_y+1
-		dx_l(i,j)=(  u(i+1,j) - ( 2 * u(i,j) ) + u(i-1,j) ) / ( x_f(i+1) - x_f(i) ) ** 2 
-		
-		end do
-	    end do
-
-	    do i=0,n_x
-		do j=0,n_y+1
-		dy_l(i,j)=( u(j,i+1) - ( 2 * u(i,j)  ) + u(j-1,i) ) / ( ( x_f(j+1) - x_f(j) ) ** 2 )
-		
-		end do
-	    end do
-
-
-	end subroutine linear 
-
-
-end module discr
+end module module_discr
