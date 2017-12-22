6a46d89c90a58b562989f9519b6cf7fe9e82f414
Chips2m <34765478+Chips2m@users.noreply.github.com>
2017-12-22 09:55:19 +0100

    Update ns2d.f90

--- ns2d.f90
+++ ns2d.f90
@@ -1,5 +1,9 @@
 program main
-
-
+  use module_mesh
+  implicit none
+  type(tmesh) :: msh
+  
+  call tmesh_allocate(self=msh,xmin=0.0,xmax=1.0,nx=10,ymin=0.0,ymax=1.0,ny=10)
+ 
 
 end program main
